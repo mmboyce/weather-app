211f7ff103194712feeab7735139669d5aa79cad
mmboyce <mathieumimmsboyce@gmail.com>
2020-03-09 16:17:47 -0400

    Add handlers, state, and constructor.

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,35 +2,85 @@ import React from 'react'
 import './styles/Weather.css'
 
 class Weather extends React.Component {
-  // TODO: Implement state to grab metric or imperial for units
+  constructor (props) {
+    super(props)
 
-  render () {
-    const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY
-
-    async function getData (cityName, units) {
-      const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherApiKey}&units=${units}`
-      const response = await fetch(apiURL, { mode: 'cors' })
-
-      console.log(response)
-
-      let data
-
-      if (response.ok) {
-        data = await response.json()
-        return data
-      } else {
-        throw new Error(response.status)
-      }
+    this.state = {
+      cityName: 'New York',
+      units: 'imperial',
+      retrieved: false
     }
 
+    this.handleData = this.handleData.bind(this)
+    this.handleWeather = this.handleWeather.bind(this)
+    this.handleError = this.handleError.bind(this)
+    this.handleUnits = this.handleUnits.bind(this)
+    this.handleSearch = this.handleSearch.bind(this)
+  }
+
+  handleUnits (units) {
+    this.setState(
+      { units }
+    )
+  }
+
+  handleWeather (json) {
+    console.log(json)
+  }
+
+  handleError (err) {
+    alert(err)
+  }
+
+  async handleData () {
+    const { cityName, units } = this.state
+
+    const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY
+
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherApiKey}&units=${units}`
+    const response = await fetch(apiURL, { mode: 'cors' })
+
+    console.log(response)
+
+    let data
+
+    if (response.ok) {
+      data = await response.json()
+      return data
+    } else {
+      throw new Error(response.status)
+    }
+  }
+
+  handleSearch (event) {
+    const { keyCode } = event
+
+    if (keyCode === 13) {
+      const cityName = event.target.value
+
+      this.setState(
+        { cityName }
+      )
+
+      this.handleData(this.state).then(this.handleWeather).catch(this.handleError)
+    }
+  }
+
+  render () {
     return (
       <div id="weather-container">
+        <div>
+          <input type="radio" name="units" id="celsius" value="metric" onClick={
+            (e) => this.handleUnits(e.target.value)
+          }/>
+          <label htmlFor="celsius">°C</label>
+          <input type="radio" name="units" id="farenheit" value="imperial" onClick={
+            (e) => this.handleUnits(e.target.value)
+          }/>
+          <label htmlFor="farenheit">°F</label>
+        </div>
         <input type="text" id="search" onKeyDown={(e) => {
-          if (e.keyCode === 13) {
-            // TODO: use .then() to update state with JSON
-            getData(e.target.value, 'imperial').then(console.log).catch(
-              (err) => console.log(err.message))
-          }
+          this.handleSearch(e)
         }}>
           {/* TODO: Use state to load gifs */}
         </input>
