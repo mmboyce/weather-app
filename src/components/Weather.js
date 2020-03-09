550327d1bbcf2dc6803e4c6839c813fa00a1ecb0
mmboyce <mathieumimmsboyce@gmail.com>
2020-03-09 15:36:33 -0400

    Refactor to extend React Component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,35 +1,42 @@
 import React from 'react'
 import './styles/Weather.css'
 
-const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY
+class Weather extends React.Component {
+  // TODO: Implement state to grab metric or imperial for units
 
-async function getData (cityName, units) {
-  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherApiKey}&units=${units}`
-  const response = await fetch(apiURL, { mode: 'cors' })
+  render () {
+    const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY
 
-  console.log(response)
+    async function getData (cityName, units) {
+      const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherApiKey}&units=${units}`
+      const response = await fetch(apiURL, { mode: 'cors' })
 
-  let data
+      console.log(response)
 
-  if (response.ok) {
-    data = await response.json()
-    return data
-  } else {
-    throw new Error(response.status)
+      let data
+
+      if (response.ok) {
+        data = await response.json()
+        return data
+      } else {
+        throw new Error(response.status)
+      }
+    }
+
+    return (
+      <div id="weather-container">
+        <input type="text" id="search" onKeyDown={(e) => {
+          if (e.keyCode === 13) {
+            // TODO: use .then() to update state with JSON
+            getData(e.target.value, 'imperial').then(console.log).catch(
+              (err) => console.log(err.message))
+          }
+        }}>
+          {/* TODO: Use state to load gifs */}
+        </input>
+      </div>
+    )
   }
-}
-
-function Weather () {
-  return (
-    <div id="weather-container">
-      <input type="text" id="search" onKeyDown={(e) => {
-        if (e.keyCode === 13) {
-          getData(e.target.value, 'imperial').then(console.log).catch(
-            (err) => console.log(err.message))
-        }
-      }}></input>
-    </div>
-  )
 }
 
 export default Weather
