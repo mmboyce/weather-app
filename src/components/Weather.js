6a68018e51fb981f36823da73a8c7b469e2307b2
mmboyce <mathieumimmsboyce@gmail.com>
2020-03-09 17:35:48 -0400

    Render weather info

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -18,18 +18,57 @@ class Weather extends React.Component {
     this.handleSearch = this.handleSearch.bind(this)
   }
 
-  handleUnits (units) {
+  handleUnits (e) {
+    const units = e.target.value
+
+    // Convert temperature if user clicks on a different unit
+    // Saves us an API call
+    if (this.state.data !== undefined) {
+      const { data } = this.state
+      const currTemp = data.main.temp
+      const storedUnits = this.state.units
+
+      if (storedUnits === 'metric' && units === 'imperial') {
+        // metric -> imperial
+        data.main.temp = (currTemp * (9 / 5)) + 32
+      } else if (storedUnits === 'imperial' && units === 'metric') {
+        // imperial -> metric
+        data.main.temp = (currTemp - 32) * (5 / 9)
+      }
+    }
+
     this.setState(
       { units }
     )
   }
 
   handleWeather (json) {
-    console.log(json)
+    this.setState(
+      {
+        retrieved: true,
+        data: json
+      }
+    )
   }
 
   handleError (err) {
-    alert(err)
+    const errCode = err.message
+    let msg
+
+    if (errCode === '429') {
+      msg = 'Too many requests!'
+    } else if (errCode === '404') {
+      msg = 'Looks like you tried a bad city name. Check your spelling :)'
+    } else {
+      msg = `Encountered an error: ${errCode}`
+    }
+
+    this.setState(
+      {
+        retrieved: null,
+        errMsg: msg
+      }
+    )
   }
 
   async handleData () {
@@ -59,31 +98,51 @@ class Weather extends React.Component {
       const cityName = event.target.value
 
       this.setState(
-        { cityName }
+        { cityName },
+        () => this.handleData().then(this.handleWeather).catch(this.handleError)
       )
-
-      this.handleData(this.state).then(this.handleWeather).catch(this.handleError)
     }
   }
 
   render () {
+    function WeatherDisplay (props) {
+      let temperature = ''
+      let weatherType = ''
+      let location = ''
+      let degrees = ''
+
+      const { retrieved, data, units, errMsg } = props
+
+      if (retrieved) {
+        temperature = data.main.temp
+        weatherType = data.weather[0].main
+        location = data.name
+        degrees = units === 'metric' ? '°C' : '°F'
+      } else if (retrieved === null) {
+        temperature = 'Oops!'
+        weatherType = errMsg
+      }
+
+      return (
+        <div id="weather-display">
+          <p>{`${temperature}${degrees}`}</p>
+          <p>{weatherType}</p>
+          <p>{location}</p>
+        </div>
+      )
+    }
+
     return (
       <div id="weather-container">
-        <div>
-          <input type="radio" name="units" id="celsius" value="metric" onClick={
-            (e) => this.handleUnits(e.target.value)
-          }/>
+        <div id="temperature-selection">
+          <input type="radio" name="units" id="celsius" value="metric" onClick={this.handleUnits}/>
           <label htmlFor="celsius">°C</label>
-          <input type="radio" name="units" id="farenheit" value="imperial" onClick={
-            (e) => this.handleUnits(e.target.value)
-          }/>
+          <input defaultChecked type="radio" name="units" id="farenheit" value="imperial" onClick={this.handleUnits}/>
           <label htmlFor="farenheit">°F</label>
         </div>
-        <input type="text" id="search" onKeyDown={(e) => {
-          this.handleSearch(e)
-        }}>
-          {/* TODO: Use state to load gifs */}
-        </input>
+        <input type="text" id="search" placeholder="New York" onKeyDown={this.handleSearch}/>
+        <WeatherDisplay retrieved={this.state.retrieved} data={this.state.data} units={this.state.units} errMsg={this.state.errMsg} />
+        {/* TODO: Use state to load gifs */}
       </div>
     )
   }
